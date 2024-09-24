7b65b3d445583beb187432ee7c1992f1d059dcd5
Sabbir-663 <ahamedsabbir663@gmail.com>
2024-09-24 21:45:54 +0600

    all input done

--- js/three-donate-btn.js
+++ js/three-donate-btn.js
@@ -85,3 +85,41 @@ document.getElementById('feni-donate-btn').addEventListener('click',function(eve
 })
 
 
+document.getElementById('quota-donate-btn').addEventListener('click',function(event){
+    event.preventDefault();
+
+    const quotaInput=document.getElementById('quota-input').value ;
+    const inputQuota=parseFloat(quotaInput);
+    const quotaBalance=document.getElementById('quota-balance').innerText ;
+    const balanceQuota=parseFloat(quotaBalance); 
+    const availableBalance=document.getElementById('available-balance').innerText;
+    const balanceAvailable=parseFloat(availableBalance);
+
+
+  //  Check if the input is a valid whole number
+    if (!/^[0-9]+$/.test(quotaInput)) {
+        alert('Please enter a valid amount');
+        return;
+    }
+
+    if (typeof inputQuota === 'number' && !isNaN(inputQuota)   && inputQuota<=balanceAvailable ) {
+
+       
+       
+      
+
+        const newQuotaBalance= inputQuota+balanceQuota;
+        document.getElementById('quota-balance').innerText=newQuotaBalance;
+
+        const newAvailableBalance=balanceAvailable-inputQuota;
+        document.getElementById('available-balance').innerText=newAvailableBalance;
+
+        alert('Thank You for donating ')
+    }
+    else{
+        alert('Insufficient Balance haha Gorib')
+    }
+    
+})
+
+
